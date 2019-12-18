80dfd65681ae7db6938fbe44950ca25c3ca936ec
Zubin Jimmy Paymaster <zubinpaymaster@yahoo.co.in>
2019-12-18 13:54:49 +0530

    Add files via upload

---
+++ 11.R
@@ -0,0 +1,17 @@
+df<-read.csv("11.csv",header=TRUE,sep=",")
+df
+summary(df)
+a<-45
+b<-35
+
+dis<-transform(df,distance=sqrt((a-df$m1)^2+(b-df$m2)^2))
+dis
+odis<-dis[order(dis$distance),]
+odis
+k<-3
+nn<-head(odis,k)
+knn<-table(nn$distance)
+nn
+knn
+t<-names(which(table(nn$grade)==max(table(nn$grade))))
+cat("grade:",t[[1]][1])
