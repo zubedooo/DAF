428df8bd66684b35f48dc8f35c75ab38ff8764ea
Zubin Jimmy Paymaster <zubinpaymaster@yahoo.co.in>
2019-12-22 11:58:08 +0530

    Update 6.r

--- 6.r
+++ 6.r
@@ -1,18 +1,36 @@
-df=read.csv("6.csv",header=TRUE,sep=",")
-df
-df$student=ifelse(df$student=='Yes',1,0)
-df$student=as.factor(df$student)
-df$default=ifelse(df$default=='Yes',1,0)
-df$default=as.factor(df$default)
-df
+library(MASS)
+library(ggplot2)
+data = read.csv("6.csv")
 
-index=sample(1:nrow(df),round(nrow(df)*0.6))
-df_train=df[index,]
-df_test=df[-index,]
-#logistic regression
-model=glm(default~student+balance+income,data = df_train,family="binomial")
+dim(data)
+str(data)
+summary(data)
 
-pred=predict(model,type = 'response',newdata = df_test)
-pred
-y_pred=ifelse(pred>0.5,1,0)
-table(y_pred,df_test$default)
+attach(data)
+data
+
+set.seed(1)
+row.number = sample(1:nrow(data),0.6*nrow(data))
+row.number
+train = data[row.number,]
+test = data[-row.number,]
+
+dim(train)
+dim(test)
+
+attach(train)
+model1 = glm(default~student+balance+income,data = train,family = binomial)
+summary(model1)
+model1
+
+
+pred.prob = predict(model1,type = "response")
+pred.prob = ifelse(pred.prob>0.5,1,0)
+table(pred.prob,train$default)
+
+attach(test)
+pred.prob = predict(model1,newdata = test,type = "response")
+pred.prob = ifelse(pred.prob>0.5,1,0)
+table(pred.prob,test$default)
+
+
